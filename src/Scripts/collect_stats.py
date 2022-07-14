b91e327c81698c020213a443d1c89efd777e2e7d
canesche <michael.canesche@gmail.com>
2022-07-14 09:43:46 -0300

    Updated: script to collect stats

--- src/Scripts/collect_stats.py
+++ src/Scripts/collect_stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, os, fnmatch, subprocess
+import sys, os, fnmatch, subprocess, json
 
 '''
     Processing data in the host terminal
@@ -11,18 +11,46 @@ def execute_process(args):
         return out.decode('utf8')
     except:
         return ""
+
+'''
+    Preprocessing json input
+    input: string with json file
+    output: json output
+'''
+def preprocessing_json(in_data):
+    s = ""
+    for l in in_data.readlines():
+        if "[" in l or "]" in l:
+            continue
+        if ":" in l and "," in l:
+            #l.split(":")
+            l = l.strip().replace(",","").split(": ")
+            s += "\"" + l[0] + "\": \"" + l[1] + "\",\n" 
+        elif ":" in l:
+            l = l.strip().split(": ")
+            s += "\"" + l[0] + "\": " 
+            if l[1] == '{':
+                s += l[1] + "\n"
+            else: 
+                s += "\"" + l[1] + "\"\n"
+        else:
+            s += l.strip() + "\n"
+    return json.loads(s)
+
 '''
     Read input data
 '''
 def read_input(in_data):
+
+    if len(in_data) < 5:
+        print("ERROR: Number of parameters")
+        print("Example to use: python3 collect_stats.py <directory> <json_file> <number_of_inputs> <opt> <stats_1> <stats_2> ... <stats_N>")
+        exit(0)
     directory = in_data[1]
-    n_inputs = int(in_data[2])
-    opt = in_data[3]
-    states = []
-    for i in range(4, len(in_data)):
-        n_state = in_data[i]
-        states.append(n_state)
-    return directory, n_inputs, states, opt
+    json_file = in_data[2]
+    n_inputs = int(in_data[3])
+    opt = in_data[4]
+    return directory, n_inputs, opt, json_file
 
 '''
     walks and saves the files inside of list
@@ -37,23 +65,18 @@ def collect_benchmark_from_directory(directory):
 '''
     print the header of CSV file
 '''
-def print_header(states):
-    print("name, ", end="")
-    for i in range(len(states)):
-        print("%s" % (states[i]), end="")
-        if (i != len(states)-1):
-            print(", ", end="")
-    print()
+def print_header():
+    print("name, blocks, phantoms, dyn inst, static inst")
 
 '''
     process the output from code executed by terminal
 '''
-def collect_stats(word, stat):
-    v = [0] * len(stat)
-    for l in word.split("\n"):
-        for i in range(len(stat)):
-            if stat[i]+":" in l:
-                v[i] = int(l.replace(stat[i]+":","").replace(" ", "").replace(",",""))
+def collect_stats(data):
+    v = [0] * len(4)
+    v[0] = data["blocks"]
+    v[1] = data["phantoms"]
+    v[2] = data["static"]["instructions"]
+    v[3] = data["dynamic"]["instructions"]
     return v
 
 '''
@@ -70,7 +93,7 @@ def sum_elements(v, pos):
 '''
 def print_solution(name, states, r):
     print(name+", ", end="")
-    for i in range(len(states)):
+    for i in range(len(r)):
         print(str(sum_elements(r, i)), end="")
         if i != len(states)-1:
             print(", ", end="")
@@ -81,16 +104,11 @@ def print_solution(name, states, r):
 '''
 if __name__ == "__main__":
 
-    if len(sys.argv) <= 4:
-        print("ERROR: Number of parameters")
-        print("Example to use: python3 collect_stats.py <directory> <number_of_inputs> <opt> <stats_1> <stats_2> ... <stats_N>")
-        exit(0)
-
-    directory, n_inputs, states, opt = read_input(sys.argv)
+    directory, n_inputs, states, opt, json_file = read_input(sys.argv)
 
     bench = collect_benchmark_from_directory(directory)
     
-    print_header(states)
+    print_header()
 
     for b in bench:
         name = b.split("/")[-1]
@@ -99,5 +117,7 @@ if __name__ == "__main__":
         for i in range(n_inputs):
             arg = "sh gen_stats.sh " + b + " " + func + " " + opt +" " + str(i)
             res = execute_process(arg)
-            r.append(collect_stats(res, states))
+            json_file = preprocessing_json("file.json")
+            r.append(collect_stats(json_file))
         print_solution(name, states, r)
+        break
